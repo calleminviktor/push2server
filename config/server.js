113467d1f6ab2781c7e227d1785b3694688daf14
calleminviktor <61667949+calleminviktor@users.noreply.github.com>
2021-02-08 14:26:40 +0100

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'd95eb161b7a0fb42a541c25591997605'),
+    },
+  },
+});
